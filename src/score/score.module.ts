8ea1e5f2337c3c9c98b6413b62ae1d4ed3e862b8
Min <minkim.dev@gmail.com>
2018-11-07 23:17:13 -0500

    fix

--- src/score/score.module.ts
+++ src/score/score.module.ts
@@ -4,11 +4,13 @@ import { Score } from './score.entity';
 import { ScoreService } from './score.service';
 import { ScoreController } from './score.controller';
 import { TeamModule } from '../team/team.module';
+import { AnswerModule } from 'answer/answer.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([Score]),
         TeamModule,
+        AnswerModule,
     ],
     providers: [ScoreService],
     controllers: [ScoreController],
