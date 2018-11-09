164d627d74a7458385212a6fcf42ceff5f994185
Min <minkim.dev@gmail.com>
2018-11-09 10:06:42 -0500

    adding socket events and emit

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -10,13 +10,32 @@ import { map } from 'rxjs/operators';
 
 @WebSocketGateway(8083)
 export class EventsGateway {
-  @WebSocketServer() server;
+  @WebSocketServer() public server;
 
   messages = [];
 
   @SubscribeMessage('events')
   onEvent(client, data): Observable<WsResponse<number>> {
     console.log('events...?');
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  }
+
+
+  @SubscribeMessage('showQuestion')
+  onShowQuestion(client, data): Observable<WsResponse<number>> {
+    this.server.emit({ event: 'showQuestion', data });
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  }
+
+  @SubscribeMessage('showAnswers')
+  onShowMusic(client, data): Observable<WsResponse<number>> {
+    this.server.emit({ event: 'showAnswers', data });
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  }
+
+  @SubscribeMessage('musicStart')
+  onMusicStart(client, data): Observable<WsResponse<number>> {
+    this.server.emit({event: 'musicStart', data});
     return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
   }
 }
