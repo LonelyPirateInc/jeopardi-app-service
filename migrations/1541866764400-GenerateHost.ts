65eaa81a3f480a8839b0574db03515a3e84b0529
Min <minkim.dev@gmail.com>
2018-11-10 11:44:20 -0500

    adding seeding

--- migrations/1541866764400-GenerateHost.ts
+++ migrations/1541866764400-GenerateHost.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class GenerateHost1541866764400 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query("INSERT INTO `user` (username, password, userType) VALUES ('anton', 'a', 'host')");
+        await queryRunner.query("INSERT INTO `user` (username, password, userType, id) VALUES ('anton', 'a', 'host', MID(UUID(),1,36))");
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
