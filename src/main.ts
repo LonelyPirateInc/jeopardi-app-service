06336cbfe5c3a4d461b1f204b3515799f4e65bce
anto0084 <anto0084@algonquinlive.com>
2018-11-10 18:00:43 -0500

    added socket address property to .env

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe());
     // app.enableCors({credentials: false});
     app.enableCors();
-    await app.listen(3000, process.env.INSTANCE_ADDRESS);
+    await app.listen(3000, process.env.SOCKET_ADDRESS);
 }
 bootstrap();
 // /**
